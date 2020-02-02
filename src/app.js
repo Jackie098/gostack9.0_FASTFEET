aa542b961e85031f7aba9f56b4c74d250495dc8b
Jackie098 <carlosmiranda19122@gmail.com>
2020-02-01 21:13:51 -0300

    Installed and configured prettier

--- src/app.js
+++ src/app.js
@@ -2,14 +2,14 @@ import express from 'express';
 import routes from './router';
 
 class App {
-  constructor(){
+  constructor() {
     this.server = express();
 
     this.middlewares();
     this.routes();
   }
 
-  middlewares(){
+  middlewares() {
     this.server.use(express.json());
   }
 
--- src/router.js
+++ src/router.js
@@ -2,6 +2,8 @@ import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ welcome: 'Welcome to my future life'}));
+routes.get('/', (req, res) =>
+  res.json({ welcome: 'Welcome to my future life' })
+);
 
 export default routes;
