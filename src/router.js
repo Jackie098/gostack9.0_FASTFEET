e32c4da270e06f16578a6d6f0f937e9c86d66231
Jackie098 <carlosmiranda19122@gmail.com>
2020-02-06 11:55:23 -0300

    Add router for store adm's and your controller, but there is a bug

--- src/router.js
+++ src/router.js
@@ -1,9 +1,23 @@
 import { Router } from 'express';
 
+import AdmController from './app/controllers/AdmController';
+
+import authMiddleware from './app/middleware/auth';
+
 const routes = new Router();
 
 routes.get('/', (req, res) =>
   res.json({ welcome: 'Welcome to my future life' })
 );
+
+/**
+ * Create new adm and session and it don't need auth
+ */
+routes.post('/adms', AdmController.store);
+
+/**
+ * Routes that need autentication
+ */
+routes.use(authMiddleware);
 
 export default routes;
