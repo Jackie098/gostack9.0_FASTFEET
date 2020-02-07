a46aca7f57df9316e24fdfc0a7bc9ff0bd502bcb
Jackie098 <carlosmiranda19122@gmail.com>
2020-02-06 22:17:54 -0300

    Created recipient model

---
+++ src/app/models/Recipient.js
@@ -0,0 +1,22 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Recipient extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        street: Sequelize.STRING,
+        number: Sequelize.INTEGER,
+        complement: Sequelize.TEXT,
+        state: Sequelize.STRING,
+        city: Sequelize.STRING,
+        cep: Sequelize.STRING,
+      },
+      {
+        sequelize,
+      }
+    );
+  }
+}
+
+export default Recipient;
