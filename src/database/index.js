6602790bf3de85712b4e497dbea0a0152e820ab0
Jackie098 <carlosmiranda19122@gmail.com>
2020-02-07 01:30:51 -0300

    Adicioned connection for 'Courier.js'

--- src/database/index.js
+++ src/database/index.js
@@ -3,10 +3,11 @@ import Sequelize from 'sequelize';
 import Adm from '../app/models/Adm';
 import Recipient from '../app/models/Recipient';
 import File from '../app/models/File';
+import Courier from '../app/models/Courier';
 
 import databaseConfig from '../config/database';
 
-const models = [Adm, Recipient, File];
+const models = [Adm, Recipient, File, Courier];
 
 class Database {
   constructor() {
@@ -16,7 +17,9 @@ class Database {
   init() {
     this.connection = new Sequelize(databaseConfig);
 
-    models.map(model => model.init(this.connection));
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
   }
 }
 
