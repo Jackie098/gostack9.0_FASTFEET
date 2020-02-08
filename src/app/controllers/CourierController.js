fcd6ffa63a469182f770060eb60a9b4525522cc9
Jackie098 <carlosmiranda19122@gmail.com>
2020-02-08 16:23:43 -0300

    Feat 'store' in 'CourierController'

--- src/app/controllers/CourierController.js
+++ src/app/controllers/CourierController.js
@@ -3,7 +3,34 @@ import Courier from '../models/Courier';
 
 class CourierController {
   async store(req, res) {
-    return res.json();
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().required(),
+      avatar: Yup.number(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const courierExists = await Courier.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (courierExists) {
+      return res.status(400).json({ error: 'Courier already exists' });
+    }
+
+    const { id, name, email, avatar_id: avatar } = await Courier.create(
+      req.body
+    );
+
+    return res.json({
+      id,
+      name,
+      email,
+      avatar,
+    });
   }
 }
 
