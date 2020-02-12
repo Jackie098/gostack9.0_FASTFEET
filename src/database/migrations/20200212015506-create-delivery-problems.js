28eda919dc17b8dd3febcc1d885eaef50503c8ad
Jackie098 <carlosmiranda19122@gmail.com>
2020-02-11 23:43:32 -0300

    Fixed bug in 'delivery-problem' migration

--- src/database/migrations/20200212015506-create-delivery-problems.js
+++ src/database/migrations/20200212015506-create-delivery-problems.js
@@ -19,11 +19,11 @@ module.exports = {
         allowNull: false,
       },
       created_at: {
-        type: Sequelize.INTEGER,
+        type: Sequelize.DATE,
         allowNull: false,
       },
       updated_at: {
-        type: Sequelize.INTEGER,
+        type: Sequelize.DATE,
         allowNull: false,
       },
     });
